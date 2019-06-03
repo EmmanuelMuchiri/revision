1df1408aa3a62843dcc7dc60f2ba4eb72d821ce5
Emmanuel Muchiri <Emmanuel.Muchiri@outlook.com>
2019-06-03 23:15:11 +0300

    added new files for my revision

---
+++ groceries.js
@@ -0,0 +1,16 @@
+var shoppingItems=prompt ("Enter your shopping list and separate with commas");
+//declared a string called shoppingItems
+var insertedItems=shoppingItems.split(",");
+//declared and assigned a new variable from the previous variable shoppingItems sort of inheritance
+//converted the string to an array using this javascript method .split() and eliminating the separator
+var sortedItems=insertedItems.sort();
+//declared a new variable sortedItems re-assigned from the previous variable insertedItems
+//sorted the items in the array using a javascript method .sort()
+sortedItems.forEach(function(item)
+//implementing the forEach loop and a callback function (.forEach) with parameters item declared
+//to be used inside the function only
+{
+console.log(item);
+});
+//opening curly brace to mark the beginning of the callback function
+//then output the item and closes the curly brace,parenthesis and the semi colon to mark the end of the program
---
+++ wordplay.js
@@ -0,0 +1,18 @@
+var isertedWords=prompt ("Enter your sentence while spacing");
+//Prompts the user to enter words while spacing in between
+var convertedWords=isertedWords.split(" ");
+//converts the inserted words to an array using the .split(" ")function
+//the parameters implies what separates the words in the sentence
+//in our case spaces
+var newArray = convertedWords.filter(word => word.length > 3);
+//a new variable is declared called newArray
+//there is a javascript method called.filter which is used to check whether the criteria defined has been met
+//for our case if the letters forming the words in the array are greater than three
+{
+console.log(newArray);
+//outputs only those words whose letters are greater than three
+console.log(newArray.join("-"));
+//.join is a javascript method that reverts back the initial data
+//type and the parameters splitted by either space or hyphen or comma depending on your prefered choice
+
+}
